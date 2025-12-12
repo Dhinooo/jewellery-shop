23208405dd1fd795235d3e57f0365a7c09fb9b87
Dhina <you@example.com>
2025-12-12 12:56:33 +0530

    Event-handler

---
+++ Dom/manipulatingElements.js
@@ -0,0 +1,23 @@
+function manipulatingElement () {
+  //   let newListElement = document.createElement('li')
+
+  //   newListElement.innerText = '<a>Link</a>'
+  //   newListElement.innerHTML = '<a>Link</a>'
+  //   newListElement.textContent = '<a>Link</a>'
+
+  //   console.log(newListElement)
+
+  let newListElement = document.createElement('li')
+
+  let input = document.querySelector('input')
+
+  newListElement.innerText = input.value
+
+  //   console.log(newListElement)
+
+  let orderList = document.querySelector('ol')
+
+//   orderList.append('Text Node',newListElement)
+
+  orderList.appendChild('Text Node',newListElement)
+}
